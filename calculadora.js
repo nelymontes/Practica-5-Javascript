52000979abb9b744a98f6ade1ff02769d134af4b
Nely Montes <njmontesdias@gmail.com>
2024-05-23 20:58:39 -0500

    ejercicios calculadora1

--- calculadora.js
+++ calculadora.js
@@ -3,4 +3,9 @@ const mostrarValor = () => {
    const valor1 = document.getElementById("valor1")
    console.log(valor1.value)
     console.log("mostrando valor")
+    const span = document.getElementById("input-valor")
+console.log(span)
+console.log(span.innerHTML) 
+span.innerHTML = "hola"
 }
+
